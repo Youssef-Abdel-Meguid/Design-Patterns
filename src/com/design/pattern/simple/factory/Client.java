8f99c22fc64b953a32632c45c6cff8cfc18cb950
unknown <yousef.dev.c@gmail.com>
2019-03-06 13:04:43 +0200

    Simple Factory Design Pattern

---
+++ src/com/design/pattern/simple/factory/Client.java
@@ -0,0 +1,21 @@
+package com.design.pattern.simple.factory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Post post = PostFactory.createPost("news");
+		
+		System.out.println(post + "\n");
+		
+		post = PostFactory.createPost("product");
+		
+		System.out.println(post + "\n");
+		
+		post = PostFactory.createPost("blog");
+		
+		System.out.println(post);
+		
+	}
+
+}
---
+++ src/com/design/pattern/simple/factory/Post.java
@@ -0,0 +1,58 @@
+package com.design.pattern.simple.factory;
+
+import java.time.LocalDateTime;
+
+public abstract class Post {
+	
+	private int id;
+	private String title;
+	private String content;
+	private LocalDateTime createdOn;
+	private LocalDateTime publishedOn;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public LocalDateTime getCreatedOn() {
+		return createdOn;
+	}
+	
+	public void setCreatedOn(LocalDateTime createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+	public LocalDateTime getPublishedOn() {
+		return publishedOn;
+	}
+	
+	public void setPublishedOn(LocalDateTime publishedOn) {
+		this.publishedOn = publishedOn;
+	}
+	
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + ", content=" + content + ", createdOn=" + createdOn
+				+ ", publishedOn=" + publishedOn + "]";
+	}
+}
